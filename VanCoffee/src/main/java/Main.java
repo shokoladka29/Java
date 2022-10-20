c956382e7be40a4805bf207614286fada1bb3b63
shokoladka29 <113143968+shokoladka29@users.noreply.github.com>
2022-10-20 13:00:16 +0300

    Add files via upload

---
+++ VanCoffee/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import static controller.MenuController.menu;
+
+public class Main {
+
+    public static void main (String[] args){
+        menu();
+
+    }
+}
---
+++ VanCoffee/src/main/java/Service/constants/MainMenu.java
@@ -0,0 +1,9 @@
+package Service.constants;
+
+public interface MainMenu {
+    int ADD_COFFEE = 1;
+    int FIND = 2;
+    int SORT = 3;
+    int CARRIER = 4;
+    int EXIT = 0;
+}
